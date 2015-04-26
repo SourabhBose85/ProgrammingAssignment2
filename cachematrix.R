2e6f0e9e0296eec90960428027eef66c90c9e0bd
SourabhBose85 <sourabhbose186@gmail.com>
2015-04-26 21:34:27 +0530

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions take matrix as input and provides inverse as output
+## It checks if inverse has been caculated for the matrix, if not then caulcates the inverse
 
-## Write a short comment describing this function
+## The below function takes matrix as input or sets the value of inverse. 
+## It also sets the inverse value of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y){
+		x <<- y
+	}
+	get <- function() x
+	setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+	list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Below function checks if inverse of matrix is present. If present it returns the inverse.
+## If not present it calculates the inverse and returns the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
